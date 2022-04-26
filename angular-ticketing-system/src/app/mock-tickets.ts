c897ac9831f73e1da6181e65a9a1e23d8cb91f97
bbice91 <bbice91@gmail.com>
2022-04-26 10:48:56 -0400

    finished runtime issue in where I wasn't providing resolution to the mock data

--- angular-ticketing-system/src/app/mock-tickets.ts
+++ angular-ticketing-system/src/app/mock-tickets.ts
@@ -1,9 +1,9 @@
 import { Ticket } from './ticket';
 
 export const TICKETS: Ticket[] = [
-  { id: 1, createdBy: 'testCreatedBy',resolvedBy:'testResolvedBy',userName:'testUserName',userEmail:'testUserEmail'},
-  { id: 2, createdBy: 'testCreatedBy2',resolvedBy:'testResolvedBy2',userName:'testUserName2',userEmail:'testUserEmail2'},
-  { id: 3, createdBy: 'testCreatedBy3',resolvedBy:'testResolvedBy3',userName:'testUserName3',userEmail:'testUserEmail3'},
-  { id: 4, createdBy: 'testCreatedBy4',resolvedBy:'testResolvedBy4',userName:'testUserName4',userEmail:'testUserEmail4'}
+  { id: 1, createdBy: 'testCreatedBy',resolvedBy:'testResolvedBy',userName:'testUserName',userEmail:'testUserEmail',resolution:'resolution1'},
+  { id: 2, createdBy: 'testCreatedBy2',resolvedBy:'testResolvedBy2',userName:'testUserName2',userEmail:'testUserEmail2',resolution:'resolution2'},
+  { id: 3, createdBy: 'testCreatedBy3',resolvedBy:'testResolvedBy3',userName:'testUserName3',userEmail:'testUserEmail3',resolution:'resolution3'},
+  { id: 4, createdBy: 'testCreatedBy4',resolvedBy:'testResolvedBy4',userName:'testUserName4',userEmail:'testUserEmail4',resolution:'resolution4'}
 
 ];
